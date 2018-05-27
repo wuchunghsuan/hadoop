1babff05e1771c8e0658b0650c23bd5bbba0f0b7
wuchunghsuan <wuchunghsuan@gmail.com>
2018-05-27 11:41:30 +0800

    Do not use slow-start.

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/rm/RMContainerAllocator.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-app/src/main/java/org/apache/hadoop/mapreduce/v2/app/rm/RMContainerAllocator.java
@@ -586,8 +586,9 @@ public class RMContainerAllocator extends RMContainerRequestor
     
     //check for slow start
     if (!getIsReduceStarted()) {//not set yet
-      int completedMapsForReduceSlowstart = (int)Math.ceil(reduceSlowStart * 
-                      totalMaps);
+      // int completedMapsForReduceSlowstart = (int)Math.ceil(reduceSlowStart * 
+      //                 totalMaps);
+      int completedMapsForReduceSlowstart = totalMaps;
       if(completedMaps < completedMapsForReduceSlowstart) {
         LOG.info("Reduce slow start threshold not met. " +
               "completedMapsForReduceSlowstart " + 
